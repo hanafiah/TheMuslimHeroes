960a18ecee6208c5dfe2a3c9210bfd31e49cf9f2
Khalid <mohammad.khalid.hussain@gmail.com>
2015-08-12 10:05:05 +0800

    Added controller for events

---
+++ application/controllers/c_events.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @author Mohammad Khalid Hussain <mohammad.khalid.hussain@gmail.com>
+ * @since 12/08/2015
+ */
+
+class Events extends CI_Controller {
+	public $view_data;
+	
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_events');
+        $this->load->helper('url');
+    }
+	
+    public function index()
+    {
+        $this->view_data['events'] = $this->m_events->get_events();
+        $this->load->view('v_index', $this->view_data);
+    }
+	
+    public function new_event()
+    {
+        if (isset($_POST['btnAdd'])) {
+            $this->m_events->insert();
+            redirect('events');
+        }
+        $this->load->view('v_new_event', $this->view_data);
+    }
+	
+    public function edit($id=FALSE)
+    {
+        if (isset($_POST['btnEdit'])) {
+            $this->m_events->update();
+            redirect('events');
+        }
+        
+        $this->view_data['event'] = $this->m_events->get_user($id);
+        $this->load->view('v_edit_event', $this->view_data);
+    }
+    
+    public function delete($id = FALSE)
+    {
+        $this->m_events->delete($id);
+        redirect('events');
+    }
+}
